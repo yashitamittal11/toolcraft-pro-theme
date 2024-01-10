c043d2b926b79c198e232567346fdb5465be5786
yashita.mittal <yashita.mittal@a8c.com>
2024-01-10 17:53:22 +0530

    Original theme files from block generator

---
+++ patterns/hidden-404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: 404
+ * Slug: toolcraft-pro/404
+ * Inserter: no
+ *
+ * @package toolcraft-pro
+ * @since 1.0.0
+ */
+?>
+<!-- wp:heading {"textAlign":"center", "level": 1} -->
+<h1 class="wp-block-heading has-text-align-center">
+	<?php esc_html_e( 'Page not found', 'toolcraft-pro' ); ?>
+</h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'toolcraft-pro' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'Search form label', 'toolcraft-pro' ); ?>","showLabel":false,"buttonText":"<?php echo esc_html_x( 'Search', 'Search form submit button text', 'toolcraft-pro' ); ?>"} /-->
